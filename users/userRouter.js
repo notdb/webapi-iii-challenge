accbc9d884d87029335951b5aa355e0fbe475acf
Doug <me@douglasbowen.com>
2019-05-29 17:36:33 -0400

    add validateUserId middleware

--- users/userRouter.js
+++ users/userRouter.js
@@ -18,7 +18,18 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:id", (req, res) => {});
+router.get("/:id", validateUserId, async (req, res) => {
+  try {
+    const users = await dbUser.getById(req.params.id);
+
+    res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "error retrieving the user"
+    });
+  }
+});
 
 router.get("/:id/posts", (req, res) => {});
 
@@ -28,7 +39,16 @@ router.put("/:id", (req, res) => {});
 
 //custom middleware
 
-function validateUserId(req, res, next) {}
+async function validateUserId(req, res, next) {
+  const user = await dbUser.getById(req.params.id);
+  console.log(user);
+
+  if (user === undefined) {
+    res.status(404).json({ message: "user not found" });
+  } else {
+    next();
+  }
+}
 
 function validateUser(req, res, next) {}
 
