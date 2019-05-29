564ba5d6f32d5ce3f1a038ebab35f1a6c0a9fea8
Doug <me@douglasbowen.com>
2019-05-29 16:16:35 -0400

    add /users endpoint

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const userRouter = require("./users/userRouter.js");
 
 const server = express();
+
+server.use(express.json());
+server.use("/users", userRouter);
 
 server.get("/", logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,37 @@
-const express = 'express';
+const express = "express";
+const dbUser = require("./userDb.js");
+const router = require("express").Router();
 
-const router = express.Router();
+router.post("/", (req, res) => {});
 
-router.post('/', (req, res) => {
+router.post("/:id/posts", (req, res) => {});
 
+router.get("/", async (req, res) => {
+  try {
+    const user = await dbUser.get(req.query);
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "error retrieving the users"
+    });
+  }
 });
 
-router.post('/:id/posts', (req, res) => {
+router.get("/:id", (req, res) => {});
 
-});
+router.get("/:id/posts", (req, res) => {});
 
-router.get('/', (req, res) => {
+router.delete("/:id", (req, res) => {});
 
-});
-
-router.get('/:id', (req, res) => {
-
-});
-
-router.get('/:id/posts', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put("/:id", (req, res) => {});
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+function validateUserId(req, res, next) {}
 
-};
+function validateUser(req, res, next) {}
 
-function validateUser(req, res, next) {
-
-};
-
-function validatePost(req, res, next) {
-
-};
+function validatePost(req, res, next) {}
 
 module.exports = router;
